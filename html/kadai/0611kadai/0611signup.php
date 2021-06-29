d79478f822367d99bc64af971814cf307e54298f
岩井峰人 <iwaiminehito@iwaipounin-no-MacBook-Pro.local>
2021-06-29 22:30:07 +0900

    エラーメッセージ修正中

--- html/kadai/0611kadai/0611signup.php
+++ html/kadai/0611kadai/0611signup.php
@@ -1,40 +1,73 @@
 <?php
 session_start();
-// postであればリダイレクトする
-// postの時はformの値が送信されている時
-// getの時は送信されていない時なのでリダイレクトしない
-// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//   header('Location:0611signup.php');
-// }
-
-// ログインできなかったらリダイレクトで新規登録画面に遷移させる
-// if (!empty($_POST['btn_submit']) || !isset ($row['username'])) {
-// header('Location: 0611register-01.php');
-// }
-
-// 条件分岐で
-// flagが1つまりusernameが空であればエラーを出力
-// flagが2つまりusernameあるけどdbと合致しなしなければ新規登録に遷移
-
-if (empty ($_POST['username'])) {
-  $flag = 1;
-} elseif (!empty ($_POST['username']) || !isset ($row['username'])) {
-  $flag = 2;
-}
 
 $dsn = 'mysql:host=mysql;dbname=test;charset=utf8';
 $user = "test";
 $password = "test";
 
-// 最初からエラーメッセージが出力されるのは、全てif文を通ってしまっているから
+// サブミットされていて、かつ、入力なければ
+// サブミットにヌル含め何か入っていたら・・・
+if () {
+  $msg_flag = 1;
+}
+// もしフラグに1が入っていたら
+if (!empty($_POST['btn_submit']) || empty($_POST['username'])) {
+  $error_message[] = "ユーザー名を入力してください";
+}
 
-$error_message[] = "ユーザー名を入力してください";
+if (empty($error_message)) {
+  // ユーザー名の文字数制限
+  $limitMax = 16;
+  $limitMin = 6;
+  $namelength = mb_strlen($_POST['username']);
 
-// if (!empty($_POST['btn_submit']) || empty($_POST['username'])) {
-//   $error_message[] = "ユーザー名を入力してください";
-// }
+  if ($namelength < $limitMin || $namelength > $limitMax) {
+    $error_message[] = '6文字以上、16文字以下で入力して下さい';
+  }
+}
+
+// もしエラーメッセージが入っていなかったら下を実行
+if (!empty($error_message)) {
+  if( !empty($_POST['btn_submit']) ) {
+    $clean = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
+
+    try {
+      $pdo = new PDO ($dsn, $user, $password);
+
+      $sql = 'SELECT * FROM users WHERE username = :username';
+      
+      $stmt = $pdo->prepare($sql);
+
+      $stmt->bindValue(':username', $clean, PDO::PARAM_STR);
+
+      $stmt -> execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    } catch (PDOException $e) {
+      $errorMessage = 'データベースエラー';
+      $e->getMessage(); 
+      $error_message[] = $e->getMessage();
+    }
+
+    // usernameがDB内に存在しているか確認
+    if (!isset ($row['username'])) {
+      $error_message[] = 'メールアドレス又はパスワードが間違っています。';
+    } else {
+      $flag = 1;
+    }
+
+    //パスワード確認後sessionにusernameを渡す
+    if ($flag) {
+      // session_regenerate_id(true); //session_idを新しく生成し、置き換える
+      $_SESSION['USERNAME'] = $row['username'];
+      // 遷移する
+      header('Location: ./0611top.php');
+      exit();
+    }
+  }
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -44,7 +77,6 @@ $error_message[] = "ユーザー名を入力してください";
   <title>ログイン画面</title>
 </head>
 <body>
-
 <h1>ログイン画面</h1>
 <?php if( !empty($error_message) || !empty($_POST['btn_submit'])): ?>
     <ul>
@@ -53,91 +85,33 @@ $error_message[] = "ユーザー名を入力してください";
 		<?php endforeach; ?>
     </ul>
 <?php endif;?>
-
-<?php 
-$limitMax = 16;
-$limitMin = 6;
-$namelength = mb_strlen($_POST['username']);
-if ($namelength > 10 || $namelength < 5) {
- echo '6文字以上、16文字以下で入力して下さい';
-}
-?>
-
-<section>
-  <?php if( !empty($message_array) ){ ?>
-    <?php foreach( $message_array as $value ){ ?>
-      <article>
-          <div class="info">
-            <h2><?php echo htmlspecialchars($value['view_name'], ENT_QUOTES, 'UTF-8'); ?></h2>
-            <time><?php echo date('Y年m月d日 H:i', strtotime($value['post_date'])); ?></time>
-          </div>
-          <p><?php echo nl2br( htmlspecialchars( $value['message'], ENT_QUOTES, 'UTF-8') ); ?></p>
-      </article>
+  <section>
+    <?php if( !empty($message_array) ){ ?>
+      <?php foreach( $message_array as $value ){ ?>
+        <article>
+            <div class="info">
+              <h2><?php echo htmlspecialchars($value['view_name'], ENT_QUOTES, 'UTF-8'); ?></h2>
+              <time><?php echo date('Y年m月d日 H:i', strtotime($value['post_date'])); ?></time>
+            </div>
+            <p><?php echo nl2br( htmlspecialchars( $value['message'], ENT_QUOTES, 'UTF-8') ); ?></p>
+        </article>
+      <?php } ?>
+    <?php } else {?>
+      <form name="loginForm" action="" method="POST">
+        <fieldset>
+          <legend>ログインフォーム</legend>
+            ユーザー名：<input type="text" name="username" placeholder="ユーザー名を入力" value="">
+            <br>
+            <!-- メールアドレス：<input type="email" name="email" placeholder="メールアドレスを入力" value="" >
+            <br> -->
+          <input type="submit" name="btn_submit" value="ログイン">
+        </fieldset>
+      </form>
+      <a href="0611register-01.php">新規登録はこちらから</a>
     <?php } ?>
-  <?php } else {?>
-    <form name="loginForm" action="" method="POST">
-      <fieldset>
-        <legend>ログインフォーム</legend>
-          ユーザー名：<input type="text" name="username" placeholder="ユーザー名を入力" value="">
-          <br>
-          <!-- メールアドレス：<input type="email" name="email" placeholder="メールアドレスを入力" value="" >
-          <br> -->
-        <input type="submit" name="btn_submit" value="ログイン">
-      </fieldset>
-    </form>
-    <a href="0611register-01.php">新規登録はこちらから</a>
-  <?php } ?>
-</section>
+  </section>
 </body>
 </html>
-
-<?php 
-if( !empty($_POST['btn_submit']) ) {
-  $clean = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
-  // 入力されたスクリプトは実行されないがDBには、「<」が「&lt;」に変換されずに記号のまま登録されている。DB側でもサニタイズしなければならないはず。
-  // そもそもサニタイズとエスケープ処理とは違うのか？＝＞エスケープはサニタイズの一種
-  // preg_replace( '/\\r\\n|\\n|\\r/', '', $_POST['username']);
-
-  try {
-    $pdo = new PDO ($dsn, $user, $password);
-
-    $sql = 'SELECT * FROM users WHERE username = :username';
-    
-    $stmt = $pdo->prepare($sql);
-
-    $stmt->bindValue(':username', $clean, PDO::PARAM_STR);
-
-    $stmt -> execute();
-
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-  } catch (PDOException $e) {
-    $errorMessage = 'データベースエラー';
-    $e->getMessage(); 
-    echo $e->getMessage();
-  }
-
-  // usernameがDB内に存在しているか確認
-  if (!isset ($row['username'])) {
-    echo 'メールアドレス又はパスワードが間違っています。';
-    exit;
-    //プログラム止める excit redirect
-  } else {
-    $flag = 1;
-  }
-
-  //パスワード確認後sessionにusernameを渡す
-  if ($flag) {
-    // session_regenerate_id(true); //session_idを新しく生成し、置き換える
-    $_SESSION['USERNAME'] = $row['username'];
-    echo 'ログインしました';
-  } else {
-    echo 'メールアドレス又はパスワードが間違。';
-    //セッションできなかったら、サインアップ画面を再表示させる
-  }
-}
-?>
-
 <!-- 
   ＄rowにフェッチで取得したデータを保持していればif文を通す
   issetは変数が宣言されていれば、またnullでなければtrueを返す
@@ -151,4 +125,5 @@ if( !empty($_POST['btn_submit']) ) {
   ・文字数制限
   サニタイズ
   リダイレクトしてログイン成功メッセージ
+
  -->
